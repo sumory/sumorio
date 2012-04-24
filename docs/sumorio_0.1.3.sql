1a0209457c9620f5007931217ef15a6a4b858f96
sumory wu <sumory.wu@gmail.com>
2012-04-24 19:06:07 +0800

    更新文档

--- docs/sumorio_0.1.3.sql
+++ docs/sumorio_0.1.3.sql
@@ -1,10 +1,10 @@
 /*
 MySQL Data Transfer
-Source Host: 192.168.20.110
+Source Host: 192.168.1.116
 Source Database: sumorio
-Target Host: 192.168.20.110
+Target Host: 192.168.1.116
 Target Database: sumorio
-Date: 2012/4/1 18:28:34
+Date: 2012/4/24 18:50:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `archive` (
   `last_reply_at` datetime DEFAULT '1970-01-01 00:00:00',
   `content_is_html` int(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for archive_category
@@ -52,7 +52,7 @@ CREATE TABLE `category` (
   `sequence` int(4) DEFAULT '0',
   `user_id` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for follow
@@ -64,6 +64,20 @@ CREATE TABLE `follow` (
   PRIMARY KEY (`user_id`,`following_id`),
   KEY `index_follow_id` (`user_id`,`following_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT '0' COMMENT '消息类型',
+  `user_id` int(11) DEFAULT '0' COMMENT '此条信息的接收者',
+  `content` varchar(300) DEFAULT '',
+  `create_at` datetime DEFAULT '1970-01-01 00:00:00',
+  `is_read` bit(1) DEFAULT b'0' COMMENT '0表示未读，1表示已读',
+  PRIMARY KEY (`id`),
+  KEY `index_message_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for nav
@@ -87,7 +101,7 @@ CREATE TABLE `reply` (
   `update_at` datetime DEFAULT '1970-01-01 00:00:00',
   `content_is_html` int(11) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
@@ -101,9 +115,10 @@ CREATE TABLE `user` (
   `avatar` varchar(255) DEFAULT '/img/avatar.png',
   PRIMARY KEY (`id`),
   KEY `index_user_id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
-INSERT INTO `user` VALUES ('14', 'sumory', 'e10adc3949ba59abbe56e057f20f883e', 'sumory.wu@gmail.com', '2012-04-01 10:27:58', 'http://www.gravatar.com/avatar/93e212fcdf7e1b0199f3ccbad0a27c13?size=48');
+
+INSERT INTO `user` VALUES ('10', 'sumory', 'e10adc3949ba59abbe56e057f20f883e', 'sumory@gg.com', '2012-03-14 10:11:55', '/img/avatar.png');
